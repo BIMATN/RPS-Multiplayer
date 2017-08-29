0980624b0d608aba14f052b0948e04cd91faed32
bimatn <bimatn@outlook.com>
2017-08-28 17:11:55 -0700

    images and html and files initialized

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+//RPS Multiplater Logic
+
+/*----------------------------VARIABLES-----------------------------------*/
+
+var player1={
+	name:"",
+	choice:,
+	wins:0,
+	losses:0,
+	ties:0
+};
+var player2={
+	name:"",
+	choice:,
+	wins:0,
+	losses:0,
+	ties:0
+};
+
+var rps = ["rock", "paper", "scissors"];
