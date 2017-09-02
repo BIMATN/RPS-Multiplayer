a8bca7079a35d70777c502829bd34c51792643cb
Benjamin Rodriguez <bimatn@outlook.com>
2017-09-02 15:09:17 -0700

    cleaned up variable use and started passing arguments to functions, still not scoring yet

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,8 +4,6 @@
 
 var player1;//player 1 object reflects database
 var player2;//player 2 object reflects database
-var hero;
-var villain;
 var gameCode = '<br><div class="row"><div class="col-4"><div class="card mx-auto" style="width:20rem;border:2px solid grey"><div class="carousel slide" data-ride="carousel" data-interval="2000" id="carouselExampleIndicators"><div class="carousel-inner"><div class="carousel-item active"><img alt="First slide" class="d-block rpsPics w-100" src="assets/images/rps-rock.png" data-value="rock"></div><div class="carousel-item"><img alt="Second slide" class="d-block rpsPics w-100" src="assets/images/rps-paper.png" data-value="paper"></div><div class="carousel-item"><img alt="Third slide" class="d-block rpsPics w-100" src="assets/images/rps-scissors.png" data-value="scissors"></div></div></div><div class="card-body"><h4 class="text-center card-title" id="playerWeaponChoice">Choose Your Weapon</h4></div><ul class="list-group list-group-flush" style="color:#fff"><li class="list-group-item bg-success" id="wins">Wins:<li class="list-group-item bg-danger" id="losses">Losses:<li class="list-group-item bg-primary" id="ties">Ties:</ul></div></div><div class="col-4"><div class="card" style="margin-top:20%;border:2px solid grey"><h2 class="text-center">Chat Room</h2><div class="form-group"><textarea class="form-control" id="chatBox" readonly rows="10"></textarea></div><div class="input-group mb-2 mb-sm-0 mr-sm-2"><div class="input-group-addon">Chat</div><input class="form-control" id="chatWrite" placeholder="Witty Banter Goes Here"> <button class="btn btn-primary" type="submit">Send</button></div></div></div><div class="col-4"><div class="card mx-auto" style="width:20rem;border:2px solid grey"><img alt="rules of the game" class="card-img-top" src="assets/images/rps-howto.png"><div class="card-body"><h4 class="text-center card-title">Opponent Choosing</h4><p class="text-center card-text" id="p2Name">Oppossing Force</div><ul class="list-group list-group-flush" style="color:#fff"><li class="list-group-item bg-success" id="opWins">Wins:<li class="list-group-item bg-danger" id="opLosses">Losses:<li class="list-group-item bg-primary" id="opTies">Ties:</ul></div></div>';
 
 // Initialize Firebase
@@ -73,42 +71,42 @@ function gamePlay (){
 }
 
 //function for processing player choice
-function playerChoice(){
+function playerChoice(player){
 	
 	$(document).on("click", ".rpsPics", function (){
 		event.preventDefault();
-		hero.choice = $(this).data("value");
-		if(hero.name === "player1")
+		player.choice = $(this).data("value");
+		if(player.name === "player1")
 		{
-			database.ref('/player1').update({choice: hero.choice});
+			database.ref('/player1').update({choice: player.choice});
 			$('.carousel').carousel('pause');
-			$("#playerWeaponChoice").text("Your weapon: "+hero.choice);
+			$("#playerWeaponChoice").text("Your weapon: "+player.choice);
 		}
 		else
 		{
-			database.ref('/player2').update({choice: hero.choice});
+			database.ref('/player2').update({choice: player.choice});
 			$('.carousel').carousel('pause');
-			$("#playerWeaponChoice").text("Your weapon: "+hero.choice);
+			$("#playerWeaponChoice").text("Your weapon: "+player.choice);
 		}
 	})
 }
 
 //Function for starting game
-function loadGame() {
+function loadGame(player) {
 	
-	database.ref('/'+hero.name).onDisconnect().update({choice:"", losses:0, present:false, ties:0, wins:0});
+	database.ref('/'+player.name).onDisconnect().update({choice:"", losses:0, present:false, ties:0, wins:0});
 	$("#gameSection").html(gameCode);
 	$('.carousel').carousel();
-	database.ref('/'+hero.name).update({present :true}); //changes database status of present to true because this hero is now playerX
+	database.ref('/'+player.name).update({present :true}); //changes database status of present to true because this hero is now playerX
 }
 
-function updateStats(){
-	$("#wins").html("Wins: "+hero.wins);
-	$("#opWins").html("Wins: "+villain.wins);
-	$("#losses").html("Losses: "+hero.losses);
-	$("#opLosses").html("Losses: "+villain.losses);
-	$("#ties").html("Ties: "+hero.ties);
-	$("#opTies").html("Ties: "+villain.ties);
+function updateStats(player, opponent){
+	$("#wins").html("Wins: "+player.wins);
+	$("#opWins").html("Wins: "+opponent.wins);
+	$("#losses").html("Losses: "+player.losses);
+	$("#opLosses").html("Losses: "+opponent.losses);
+	$("#ties").html("Ties: "+player.ties);
+	$("#opTies").html("Ties: "+opponent.ties);
 }
 
 
@@ -131,60 +129,88 @@ database.ref('/player2').once("value").then(function (childSnapshot){
 $(document).on("click", "#start", function(){
 	event.preventDefault();
 	if(player1.present===false&&player2.present===false){
-		hero=player1;
-		villain=player2;
 		//player 1 Object Monitor
 		database.ref('/player1').on("value", function (childSnapshot){
 			player1=childSnapshot.val();
 			console.log(player1);
-			updateStats();
+			updateStats(player1, player2);
 		})
 		//player 2 Object Monitors
 		database.ref('/player2').on("value", function (childSnapshot){
 			player2=childSnapshot.val();
 			console.log(player2);
-			updateStats();
+			updateStats(player1, player2);
 		})
-		loadGame();
-		playerChoice();
-		gamePlay();
+		loadGame(player1);
+		playerChoice(player1);
+		if(player2.choice !== ""){
+			if(player1.choice==="rock"||player1.choice==="scissors"||player1.choice==="paper"){
+				gamePlay();
+			}
+			else{
+					database.ref('/player1/choice').on("child_changed", gamePlay());
+				}
+		}
+		else{
+			database.ref('/player2/choice').on("child_changed", function(){
+				if(player1.choice==="rock"||player1.choice==="scissors"||player1.choice==="paper"){
+					gamePlay();
+				}
+				else{
+					database.ref('/player1/choice').on("child_changed", gamePlay());
+				}
+			})
+		}
 	}
 	else if(player1.present===false&&player2.present===true){
-		hero=player1;
-		villain=player2;
 		//player 1 Object Monitor
 		database.ref('/player1').on("value", function (childSnapshot){
 			player1=childSnapshot.val();
 			console.log(player1);
-			updateStats();
+			updateStats(player1,player2);
 		})
 		//player 2 Object Monitors
 		database.ref('/player2').on("value", function (childSnapshot){
 			player2=childSnapshot.val();
 			console.log(player2);
-			updateStats();
+			updateStats(player1,player2);
 		})
-		loadGame();
-		playerChoice();
-		gamePlay();
+		loadGame(player1);
+		playerChoice(player1);
+		if(player2.choice !== ""){
+			if(player1.choice==="rock"||player1.choice==="scissors"||player1.choice==="paper"){
+				gamePlay();
+			}
+			else{
+					database.ref('/player1/choice').on("child_changed", gamePlay());
+				}
+		}
+		else{
+			database.ref('/player2/choice').on("child_changed", function(){
+				if(player1.choice==="rock"||player1.choice==="scissors"||player1.choice==="paper"){
+					gamePlay();
+				}
+				else{
+					database.ref('/player1/choice').on("child_changed", gamePlay());
+				}
+			})
+		}
 	}
 	else if(player1.present===true&&player2.present===false){
-		hero=player2;
-		villain=player1;
 		//player 1 Object Monitor
 		database.ref('/player1').on("value", function (childSnapshot){
 			player1=childSnapshot.val();
 			console.log(player1);
-			updateStats();
+			updateStats(player2,player1);
 		})
 		//player 2 Object Monitors
 		database.ref('/player2').on("value", function (childSnapshot){
 			player2=childSnapshot.val();
 			console.log(player2);
-			updateStats();
+			updateStats(player2,player1);
 		})
-		loadGame();
-		playerChoice();
+		loadGame(player2);
+		playerChoice(player2);
 	}
 	else {
 		$("#gameFull").text("This server is full. Please try again later. Thank you."); //rejects players because player1 and player2 are set
